c7f2266dc8c839df12b54a0d80e90bee9fd607bc
Sakib Sakfi <42506020+sakfi@users.noreply.github.com>
2025-01-09 13:25:37 +0600

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Index/PGD-GB Batch 2'],
+  protectedRoutes: ['/Index/PGD-GB Batch 2',],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:DaRk@sakfi.onmicrosoft.com',
