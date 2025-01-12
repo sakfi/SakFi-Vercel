106b015c2970f17cf8290cdc2184b72c7d22235a
Sakib Sakfi <42506020+sakfi@users.noreply.github.com>
2025-01-12 20:25:48 +0600

    Update site.config.js
    updated footer

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by SakFi.',
+    'Powered by <a href="https://github.com/sakfi/SakFi-Vercel/tree/main" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by SakFi.'<br><p font-size:8px">All Content is Collected and uploaded from Various Sites around the globe, proxied using Cloudflare Technology and "DaRk World" doesn’t rip/pirate anything. Copyright owners/Representatives contact us if you think any of the "DaRk World" files violate intellectual property rights . This is just an Index, much like how Google and Search Engine Works.</p>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
