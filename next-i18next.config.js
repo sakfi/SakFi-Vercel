0cc6c8c6754c69b9bcdf6229277332033b5c13ac
Sakib Sakfi <42506020+sakfi@users.noreply.github.com>
2025-01-13 15:15:47 +0600

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en', // Default language
-    locales: ['de-DE', 'en', 'es', 'fr', 'zh-CN', 'hi', 'id', 'tr-TR', 'zh-TW'], // Supported languages
+    locales: ['de-DE', 'en', 'es', 'fr', 'bn', 'zh-CN', 'hi', 'id', 'tr-TR', 'zh-TW'], // Supported languages
   },
   localePath: path.resolve('./public/locales'), // Correct path for locales
   reloadOnPrerender: process.env.NODE_ENV === 'development', // Reload during development
