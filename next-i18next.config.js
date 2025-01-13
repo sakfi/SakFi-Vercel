0d69e9c724c69ab9c99cd6d62e8d98a828f41f06
Sakib Sakfi <42506020+sakfi@users.noreply.github.com>
2025-01-13 14:38:46 +0600

    Update next-i18next.config.js
    fixed by removing fallbackLng

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,15 +1,14 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   i18n: {
     defaultLocale: 'en', // Default language
-    locales: ['de-DE', 'en', 'es', 'fr', 'zh-CN', 'hi', 'id', 'tr-TR', 'zh-TW'] // Add 'fr' for French
-    fallbackLng: 'en', // Set fallback language
+    locales: ['de-DE', 'en', 'es', 'fr', 'zh-CN', 'hi', 'id', 'tr-TR', 'zh-TW'], // Supported languages
   },
-  localePath: path.resolve('public/locales'),
-  reloadOnPrerender: process.env.NODE_ENV === 'development',
-  keySeparator: false,
-  namespaceSeparator: false,
-  pluralSeparator: '——',
-  contextSeparator: '——'
-}
+  localePath: path.resolve('./public/locales'), // Correct path for locales
+  reloadOnPrerender: process.env.NODE_ENV === 'development', // Reload during development
+  keySeparator: false, // Disable key separation
+  namespaceSeparator: false, // Disable namespace separation
+  pluralSeparator: '——', // Plural separator
+  contextSeparator: '——', // Context separator
+};
