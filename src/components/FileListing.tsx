2656b4fd5268aee1981a3bee417bc4fc070015ca
Sakib Sakfi <ananno.sakib@gmail.com>
2025-05-11 21:10:30 +0600

    fix: removed invalid loading prop from ImagePreview

--- src/components/FileListing.tsx
+++ src/components/FileListing.tsx
@@ -396,7 +396,7 @@ const FileListing: FC<{ query?: ParsedUrlQuery }> = ({ query }) => {
     if (previewType) {
       switch (previewType) {
         case preview.image:
-          return <ImagePreview loading="lazy" file={file} />;
+          return <ImagePreview file={file} />;
 
         case preview.text:
           return <TextPreview file={file} />
